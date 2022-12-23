f90ec4e1fc5cbd3c87ae3c57fa17c566e2d3e0d5
John O'Halloran <john.ohalloran@telus.com>
2022-12-22 21:03:51 -0500

    finished

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+const sum = (fromN, toN) => {
+  let total = 0; 
+
+  const count = (start, end) => {
+    total += start;
+    return start !== end && (start++, count(start, end));
+  }
+
+  count(fromN, toN);
+
+  return total;
 }
 
 module.exports = sum;
